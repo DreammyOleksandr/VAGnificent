7df43cd936674974fae6e23ea5d0c4ae0484fff1
Alex Bondarenko <albom2004q@gmail.com>
2023-04-18 00:19:00 +0300

    Table has been fixed successfuly

--- VAGnificent/wwwroot/js/brand.js
+++ VAGnificent/wwwroot/js/brand.js
@@ -1,10 +1,11 @@
+
 $(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    dataTable = $('#brandsData').DataTable({
-        "ajax": {"url": '/Admin/Brand/Index/'},
+    dataTable = $('#tblData').DataTable({
+        "ajax": {"url": "/Admin/Brand/GetAll"},
         "columns": [
             {data: "brandName", "width": "40%"},
             {data: "ceo", "width": "40%"},
@@ -12,8 +13,8 @@ function loadDataTable() {
                 data: "id",
                 "render": function (data) {
                     return `<div class="w-25 btn-group" role="group">
-                <a href="/Brand/Edit?id=${data}" class="btn btn-primary mx-2">Edit</a>
-                <a href="/Brand/Delete?id=${data}" class="btn btn-danger mx-2">Delete</a>
+                <a href="Brand/Edit?id=${data}" class="btn btn-primary mx-2">Edit</a>
+                <a href="Brand/Delete?id=${data}" class="btn btn-danger mx-2">Delete</a>
                 </div>`
                 },
                 "width":"20%"
