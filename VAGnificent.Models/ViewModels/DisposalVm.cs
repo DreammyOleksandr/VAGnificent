eb51ab43d716423a94e265c7d5eb545e4ef05a1b
Alex Bondarenko <albom2004q@gmail.com>
2023-04-19 00:03:21 +0300

    Changing Parametrs in create view

--- VAGnificent.Models/ViewModels/DisposalVm.cs
+++ VAGnificent.Models/ViewModels/DisposalVm.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using VAGnificent.Models.Models;
+
 namespace VAGnificent.Models.ViewModels;
 
 public class DisposalVm
 {
-    
+    public Disposal Disposal { get; set; }
+    public IEnumerable<SelectListItem> Brands { get; set; }
 }
