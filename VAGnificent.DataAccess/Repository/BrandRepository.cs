ed67e551ffd6d5af1b8f4a4f4885af02c0c18d8a
Alex Bondarenko <albom2004q@gmail.com>
2023-04-24 19:50:15 +0300

    Implementing missing methods of brand repo

--- VAGnificent.DataAccess/Repository/BrandRepository.cs
+++ VAGnificent.DataAccess/Repository/BrandRepository.cs
@@ -1,6 +1,23 @@
+using VAGnificent.Models.Models;
+
 namespace VAGnificent.DataAccess.Repository.IRepository;
 
-public class BrandRepository
+public class BrandRepository : Repository<Brand>, IBrandRepository
 {
-    
+    private readonly ApplicationDbContext _db;
+
+    public BrandRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public void Update(Brand brand)
+    {
+        _db.Update(brand);
+    }
+
+    public void Save()
+    {
+        _db.SaveChanges();
+    }
 }
