7d36396cee7ab534fa7705fc322f28e3e8355306
Alex Bondarenko <albom2004q@gmail.com>
2023-04-24 22:16:50 +0300

    Editing accidents input field to make it work properly

--- VAGnificent/Areas/Customer/Controllers/HomeController.cs
+++ VAGnificent/Areas/Customer/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace VAGnificent.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
