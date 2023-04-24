65e0a8d89f0e1585eff0c256b85191b68f092957
Alex Bondarenko <albom2004q@gmail.com>
2023-04-24 22:29:26 +0300

    Fixing Display names of classes` properties

--- VAGnificent.Models/Models/Brand.cs
+++ VAGnificent.Models/Models/Brand.cs
@@ -1,10 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace VAGnificent.Models.Models;
 
 public class Brand
 {
     [Key] public int Id { get; set; }
-    [Required] public string BrandName { get; set; }
+    [Required, DisplayName("Brand Name")] public string BrandName { get; set; }
     [Required] public string CEO { get; set; }
 }
--- VAGnificent.Models/Models/Disposal.cs
+++ VAGnificent.Models/Models/Disposal.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
@@ -10,26 +11,26 @@ public class Disposal
     [Key]
     public int Id { get; set; }
     
-    [ValidateNever]
+    [ValidateNever, DisplayName("Brand")]
     public int BrandId { get; set; }
     [ForeignKey("BrandId")]
     [ValidateNever]
     public Brand? Brand { get; set; }
 
     [Required] public string? Model { get; set; }
-    [Required] public string? BrandCountry { get; set; }
+    [Required, DisplayName("Country of Brand")] public string? BrandCountry { get; set; }
     [Required] public string? Colour { get; set; }
-    [Required] public string? FuelType { get; set; }
+    [Required, DisplayName("Fuel Type")] public string? FuelType { get; set; }
     public int Weight { get; set; }
-    public int HorsePower { get; set; }
-    public int EngineCapacity { get; set; }
-    [Required] public string? TransmisionType { get; set; }
-    [Required] public int TravelledDistance { get; set; }
+    [DisplayName("Horse Power")]public int HorsePower { get; set; }
+    [DisplayName("Engine Capacity")]public int EngineCapacity { get; set; }
+    [Required, DisplayName("Transmision Type")] public string? TransmisionType { get; set; }
+    [Required, DisplayName("Travelled Distance")] public int TravelledDistance { get; set; }
     [Required] public DateTime Year { get; set; }
 
-    [Required, Range(0, 999999, ErrorMessage = "Our site is not that rich to sell such expensive cars :D")]
+    [Required, Range(0, 999999, ErrorMessage = "Our site is not that rich to sell such expensive cars :D"), DisplayName("Price in Dollars")]
     public int DollarsPrice { get; set; }
 
     [Required] public bool Accidents { get; set; }
-    [ValidateNever] public string? ImageUrl { get; set; }
+    [ValidateNever, DisplayName("Image")] public string? ImageUrl { get; set; }
 }
