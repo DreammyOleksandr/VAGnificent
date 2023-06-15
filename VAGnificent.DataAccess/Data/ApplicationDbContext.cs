402d552aad9c5233d2783d0c8a3588b71eacf7ea
Alex Bondarenko <albom2004q@gmail.com>
2023-06-15 23:57:23 +0300

    Modifiing DbContext for Scuffolding an Identity

--- VAGnificent.DataAccess/Data/ApplicationDbContext.cs
+++ VAGnificent.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using VAGnificent.Models.Models;
 
 namespace VAGnificent.DataAccess;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
@@ -12,28 +14,6 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        
-        modelBuilder.Entity<Brand>().HasData(
-            new Brand { Id = 1, BrandName = "Lamborghini", CEO = "Stefan Vinkelman"});
-
-        // modelBuilder.Entity<Disposal>().HasData(
-        //     new Disposal
-        //     {
-        //         Id = 1,
-        //         BrandId = 5,
-        //         Model = "911 922 Turbo",
-        //         BrandCountry = "Germany",
-        //         Colour = "White",
-        //         FuelType = "Gazoline",
-        //         Weight = 1300,
-        //         HorsePower = 420,
-        //         EngineCapacity = 3000,
-        //         TransmisionType = "Robo",
-        //         Year = new DateTime(2021),
-        //         DollarsPrice = 100000,
-        //         Accidents = false,
-        //         TravelledDistance = 1000,
-        //     });
     }
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Disposal> Disposals { get; set; }
