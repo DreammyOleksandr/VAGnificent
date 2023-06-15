157f9fedb448218e62d26d138d448c797ee7125d
Alex Bondarenko <albom2004q@gmail.com>
2023-06-15 23:57:47 +0300

    Identity User Program changes

--- VAGnificent/Program.cs
+++ VAGnificent/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using NuGet.Protocol.Core.Types;
 using VAGnificent.DataAccess;
 using VAGnificent.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IBrandRepository, BrandRepository>();
 builder.Services.AddScoped<IDisposalRepository, DisposalRepository>();
