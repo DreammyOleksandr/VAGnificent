d1177f8f30554632051f4bbe197f537342488adb
Alex Bondarenko <albom2004q@gmail.com>
2023-06-16 00:08:57 +0300

    Making authorization possible

--- VAGnificent/Program.cs
+++ VAGnificent/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options
     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IBrandRepository, BrandRepository>();
 builder.Services.AddScoped<IDisposalRepository, DisposalRepository>();
 
@@ -34,7 +34,7 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
