354ca4162de1ad2c45b619c5e13cc7282c0cbe60
Alex Bondarenko <albom2004q@gmail.com>
2023-06-29 14:48:41 +0300

    Adding Address row to Db

--- VAGnificent.Models/Models/Order.cs
+++ VAGnificent.Models/Models/Order.cs
@@ -10,6 +10,7 @@ public class Order
 
     [Required] public string FirstName { get; set; }
     [Required] public string LastName { get; set; }
+    [Required] public string Address { get; set; }
     [Required] public string PostalCode { get; set; }
     [Required] public string State { get; set; }
     [Required] public string Country { get; set; }
