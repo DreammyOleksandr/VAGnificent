61c10fe7bddb615b3d5e4e83080127dd13744580
Alex Bondarenko <albom2004q@gmail.com>
2023-06-29 12:52:56 +0300

    Adding DataAnnotations and attributes to models

--- VAGnificent.Models/Models/Order.cs
+++ VAGnificent.Models/Models/Order.cs
@@ -1,18 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VAGnificent.Models.Models;
 
 public class Order
 {
-    public int Id { get; set; }
+    [Key] public int Id { get; set; }
 
     public List<OrderDetail> OrderDetails { get; set; }
 
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PostalCode { get; set; }
-    public string State { get; set; }
-    public string Country { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Email { get; set; }
+    [Required] public string FirstName { get; set; }
+    [Required] public string LastName { get; set; }
+    [Required] public string PostalCode { get; set; }
+    [Required] public string State { get; set; }
+    [Required] public string Country { get; set; }
+    [Required] public string PhoneNumber { get; set; }
+    [Required] public string Email { get; set; }
+
     public string OrderTotal { get; set; }
+
     public DateTime OrderPlaced { get; set; }
 }
--- VAGnificent.Models/Models/OrderDetail.cs
+++ VAGnificent.Models/Models/OrderDetail.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
 namespace VAGnificent.Models.Models;
 
 public class OrderDetail
 {
-    public int Id { get; set; }
+    [Key] public int Id { get; set; }
     public int OrderId { get; set; }
     public int DisposalId { get; set; }
     public decimal Price { get; set; }
+
+    [ForeignKey("DisposalId"), ValidateNever]
     public virtual Disposal Disposal { get; set; }
+
+    [ForeignKey("OrderId"), ValidateNever]
+    public virtual Order Order { get; set; }
 }
