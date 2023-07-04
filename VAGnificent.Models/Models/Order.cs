b41fa6c9463ec3223da2888abf38b6a950cb8ec8
Alex Bondarenko <albom2004q@gmail.com>
2023-07-04 11:45:02 +0300

    Adding validation attributes to the order model

--- VAGnificent.Models/Models/Order.cs
+++ VAGnificent.Models/Models/Order.cs
@@ -1,23 +1,55 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace VAGnificent.Models.Models;
 
 public class Order
 {
-    [Key] public int Id { get; set; }
+    [Key, BindNever] public int Id { get; set; }
 
-    public List<OrderDetail> OrderDetails { get; set; }
+    [ValidateNever]public List<OrderDetail> OrderDetails { get; set; }
 
-    [Required] public string FirstName { get; set; }
-    [Required] public string LastName { get; set; }
-    [Required] public string Address { get; set; }
-    [Required] public string PostalCode { get; set; }
-    [Required] public string State { get; set; }
-    [Required] public string Country { get; set; }
-    [Required] public string PhoneNumber { get; set; }
-    [Required] public string Email { get; set; }
+    [Required(ErrorMessage = "Enter your first name")]
+    [DisplayName("First Name")]
+    [StringLength(50)]
+    public string FirstName { get; set; }
 
-    public string OrderTotal { get; set; }
+    [Required(ErrorMessage = "Enter your last name")]
+    [DisplayName("Last Name")]
+    [StringLength(50)]
+    public string LastName { get; set; }
 
-    public DateTime OrderPlaced { get; set; }
+    [Required(ErrorMessage = "Enter your Address")]
+    [StringLength(100)]
+    public string Address { get; set; }
+
+    [Required(ErrorMessage = "Enter your postal code")]
+    [DisplayName("Postal Code")]
+    [StringLength(10, MinimumLength = 4)]
+    public string PostalCode { get; set; }
+
+    [Required(ErrorMessage = "Enter your state")]
+    [StringLength(50)]
+    public string State { get; set; }
+
+    [Required(ErrorMessage = "Enter your country")]
+    [StringLength(50)]
+    public string Country { get; set; }
+
+    [Required(ErrorMessage = "Enter your phone number")]
+    [DisplayName("Phone Number")]
+    [StringLength(20)]
+    public string PhoneNumber { get; set; }
+
+    [Required(ErrorMessage = "Enter your email")]
+    [StringLength(50)]
+    public string Email { get; set; }
+
+    [ValidateNever]public string OrderTotal { get; set; }
+
+    public DateTime OrderPlaced { get; set; } = DateTime.Now;
 }
